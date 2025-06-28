d98418452d4ec23211b12e94844a94d391b2cbc7
Louis Miguel Sabaricos <lmfsabaricos@yahoo.com>
2025-06-28 02:37:14 -0700

    Did most of the frontpage html/css

---
+++ script.js
@@ -0,0 +1,43 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: ["#FF0099", "#00FFFF"]
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.7
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00FFFF",
+      opacity: 0.5,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  },
+  retina_detect: true
+});
